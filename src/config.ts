67b461c60396ef5c19031eb5ca9448f765d1dd71
Pawel Jamroziak <pjamroziak@gmail.com>
2021-03-18 13:39:33 -0700

    build: Add base configuration consts

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const PATH_TO_FILE               : string = "./personal_data.txt";
+
+export const RABBIT_MQ_CONNECTION_URL   : string = "amqp://localhost";
+export const RABBIT_MQ_QUEUE_NAME       : string = "personal-data";
